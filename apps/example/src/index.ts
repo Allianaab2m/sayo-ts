3f2d9dfa1d4941f0fb5b50ea6c8a600bcbd8ed04
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2026-04-14 17:51:10 +0900

    feat: remove all

--- apps/example/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { SayoApp } from "@sayo-ts/core"
-import { AppApi } from "./api/api.js"
-import { UsersLive } from "./api/users.routes.js"
-
-SayoApp.make(AppApi)
-  .addHandlers(UsersLive)
-  .listen()
